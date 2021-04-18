b58cbe26e8244110a41bf8fb4049032c44eb64b1
mikepxq <1024694765@qq.com>
2021-04-19 00:36:20 +0800

    feat: 迁移到 vite 编译环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,11 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint", "prettier"],
+
   rules: {
     "prettier/prettier": ["error", prettier], //同步规则
-    "@typescript-eslint/explicit-module-boundary-types": ["warn"], //同步规则到本地运行环境
+    "@typescript-eslint/explicit-module-boundary-types": "warn", //同步规则到本地运行环境
+    "@typescript-eslint/no-unused-vars": "warn", //同步规则到本地运行环境
+    "@typescript-eslint/no-empty-interface": "warn", //同步规则到本地运行环境
   },
 };
--- 项目记录.ts
+++ 项目记录.ts
@@ -1,45 +1,31 @@
 /**
 1.基础构建
-  1.npx create-react-app react-antd-template --template typescript
-  2.npm install @craco/craco -D
+  1.npm init @vitejs/app my-vue-app --template react-ts
 2.统一环境
   1.npm i -D eslint
     ./node_modules/.bin/eslint --init
-  2.npm i -D --save-exact  prettier
+  2.npm i -D  prettier
     npm i -D eslint-plugin-prettier  使用eslint使用prettier
-    npm i -D eslint-config-prettier  解决冲突
-  3.npx mrm lint-staged  //将下载 husky and lint-staged 并添加配置 提交全局格式化
+    npm i -D eslint-config-prettier
+  3.git 格式化 提交校验
+    npm i -D husky@=4
+    npm i -D lint-staged
+    package.json 配置
   4.commitlint
     npm i -D @commitlint/config-conventional @commitlint/cli
-    "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"    提交前检查 commit 标题
+    package.json
+      "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"    提交前检查 commit 标题
+    commitlint.config.js
   5.别名
-    paths.js
-    craco.config.js webpack.alias
+    vite.config.ts
   6.less
-    npm i -D craco-less
-
-3.结构 视图 路由 样式 
+    npm i -D less
+3.搭建项目结构
+  1. 
 4. 数据结构 store mock 连通mock数据
 5.antd layout 
  */
-/**commitlint
-build
-ci
-chore
-docs
-feat
-fix
-perf
-refactor
-revert
-style
-test
- */
-/**
-参照：
-https://github.com/gsoft-inc/craco/blob/master/packages/craco/README.md#installation
-https://cn.eslint.org/docs/user-guide/getting-started
-https://prettier.io/docs/en/install.html
-https://prettier.io/docs/en/options.html
-https://www.npmjs.com/package/eslint-plugin-prettier
+
+/**参照
+vite官网--------- https://www.pipipi.net/vite/
  */
