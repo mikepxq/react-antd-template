235228e875b18ddd6478d6c672e94d59a4997a4f
mikepxq <1024694765@qq.com>
2021-11-11 00:55:56 +0800

    feat: 批量导入icons 暂时无效 去除相关代码

--- src/icons/index.ts
+++
@@ -1,7 +0,0 @@
-const reqSvgs = require.context("./svg", false, /\.svg$/);
-const requireAll = (requireContext: __WebpackModuleApi.RequireContext) =>
-  requireContext.keys().map((value) => {
-    return requireContext(value).default;
-  });
-//deving 无效
-export const initSvgIcons = () => requireAll(reqSvgs);
