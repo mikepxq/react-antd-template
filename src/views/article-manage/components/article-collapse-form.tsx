fa95a500f54ece17ae02c52921d40e3e113e5a47
mikepxq <1024694765@qq.com>
2021-11-11 00:43:46 +0800

    feat: 文章新建 60%

---
+++ src/views/article-manage/components/article-collapse-form.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Button, Col, Collapse, Form, Input, Row, Space } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+import AppInput from "@/components/app-input";
+const { Panel } = Collapse;
+interface Props {
+  form: Antd.FormInstance;
+  onPublish?: () => void;
+  onDraft?: () => void;
+}
+const ArticleCollapseForm: React.FC<ViewProps<Props>> = (props) => {
+  const { className = "", onPublish, onDraft, form } = props;
+  const [isOpen, setIsOpen] = useState(true); //默认显示表单
+  //render
+  return (
+    <Collapse className={`article-collapse-form ${className}`} activeKey={[String(isOpen)]}>
+      <Panel
+        showArrow={false}
+        header={
+          <nav className="clear">
+            <Button
+              type="primary"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}>
+              配置
+              <RightOutlined className="arrow" style={{ transform: `rotate(${isOpen ? 90 : 0}deg)` }} />
+            </Button>
+            <Space className="fr">
+              <Button onClick={onPublish} type="primary">
+                发布
+              </Button>
+              {/* TODO 当点击时 */}
+              <Button onClick={onDraft}>草稿</Button>
+            </Space>
+          </nav>
+        }
+        key="true">
+        <Form form={form} labelCol={{ span: 5 }} wrapperCol={{ span: 19 }}>
+          <Row gutter={24}>
+            <Col span={8}>
+              <Form.Item name="title" label="标题" rules={[{ required: true }]}>
+                <AppInput placeholder="请输入标题" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item name="author" label="作者" rules={[{ required: true }]}>
+                <AppInput placeholder="请输入作者" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item name="description" label="描述">
+                <Input.TextArea placeholder="请输入描述" maxLength={100}></Input.TextArea>
+              </Form.Item>
+            </Col>
+          </Row>
+          {/* <Row gutter={24}>
+            <Col span={8}></Col>
+          </Row> */}
+        </Form>
+        <footer className="clear">
+          <Button
+            className="fr"
+            onClick={() => {
+              form.resetFields();
+            }}>
+            重置
+          </Button>
+        </footer>
+      </Panel>
+    </Collapse>
+  );
+};
+export default ArticleCollapseForm;
--- src/views/article-manage/create.tsx
+++ src/views/article-manage/create.tsx
@@ -1,25 +1,25 @@
-import { Button } from "antd";
+import { Form } from "antd";
 import React from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import ContentMain from "@/console-layout/content-main";
+import ArticleCollapseForm from "./components/article-collapse-form";
 interface Props {
   [key: string]: any;
 }
 const ArticleManage: React.FC<ViewProps<Props>> = (props) => {
   const { className = "" } = props;
   const EditorRef = React.createRef<Editor>();
-  const onSaveDraft = () => {
-    const editor = EditorRef.current?.getInstance();
-    const s = editor?.getMarkdown();
-    console.log("[s]", s);
+  const onDraft = () => {
+    // const editor = EditorRef.current?.getInstance();
+    // const s = editor?.getMarkdown();
+    // console.log("[s]", s);
   };
+  const [form] = Form.useForm();
   //render
   return (
     <ContentMain className={`${className} flex-column`}>
-      <nav>
-        <Button onClick={onSaveDraft}>草稿</Button>
-      </nav>
+      <ArticleCollapseForm form={form} onDraft={onDraft} />
       <main className="all-remain" style={{ marginTop: 10 }}>
         <Editor
           initialValue="h react editor"
