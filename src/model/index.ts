c3dc019eaa19691782518ac78b3b71f092008afc
mikepxq <1024694765@qq.com>
2021-11-15 00:30:39 +0800

    feat:修复版本对比正则

--- src/model/index.ts
+++ src/model/index.ts
@@ -8,5 +8,5 @@ export const patternMap = {
   mockApi: /^\/mock/,
   /** 单字节中文 字母 数字 下划线  多用于名称*/
   alpha_number_: /^[\u4e00-\u9fa5a-zA-Z0-9_]$/,
-  appVersion: /^(\d+\.)+\d$/,
+  appVersion: /^(\d+\.)+\d+$/,
 };
