0cf54a18eef09f33153ccd5c97837cee7260c2fb
mikepxq <1024694765@qq.com>
2021-11-17 01:32:42 +0800

    feat: 文章编辑 60%

--- src/views/article-manage/update.tsx
+++ src/views/article-manage/update.tsx
@@ -4,16 +4,16 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import ContentMain from "@/console-layout/content-main";
 import ArticleCollapseForm from "./components/article-collapse-form";
-import { sleep } from "@/utils";
 import LazySpin from "@/components/lazy-spin";
 import queryString from "query-string";
-import { reqArticleInfo } from "@/apis";
-import { appMessage } from "@/plugins/antd";
+import { reqArticleInfo, reqArticleUpdate } from "@/apis";
+import { appMessage, appNotification } from "@/plugins/antd";
 import { useHistory } from "react-router-dom";
 
 interface Props {
   [key: string]: any;
 }
+type OnSubmitOptions = { publishStatus: ArticleStatus };
 const ArticleManage: React.FC<ViewProps<Props>> = (props) => {
   const { className = "" } = props;
   //初始数据
@@ -32,11 +32,9 @@ const ArticleManage: React.FC<ViewProps<Props>> = (props) => {
   //
   const getArticleInfo = async () => {
     if (!id) return;
+    // TODO完成api
     await reqArticleInfo({ id: id });
-    form.setFieldsValue({
-      title: "本地测试数据",
-      author: "本地测试数据",
-    });
+    form.setFieldsValue({ title: "本地测试数据", author: "本地测试数据" });
     setIsInitEnd(true);
   };
   useEffect(() => {
@@ -44,28 +42,20 @@ const ArticleManage: React.FC<ViewProps<Props>> = (props) => {
   }, [id]);
   //
   const EditorRef = React.createRef<Editor>();
-  const [draftLoading, setDraftLoading] = useState(false);
-  const [publishLoading, setPublishLoading] = useState(false);
-  const onDraft = async () => {
+  const [loadingMap, setLoadingMap] = useState({ draft: false, publish: false });
+
+  /** 再次保存文章 带上id */
+  const onUpdate = async (options: OnSubmitOptions) => {
     const _form = await form.validateFields().catch(() => undefined);
-    if (!_form || draftLoading) return;
-    setDraftLoading(true);
-    await sleep();
-    setDraftLoading(false);
-    // const editor = EditorRef.current?.getInstance();
-    // const s = editor?.getMarkdown();
-    // console.log("[s]", s);
+    if (!_form || loadingMap[options.publishStatus] || !EditorRef.current || id === undefined) return;
+    const editor = EditorRef.current.getInstance();
+    setLoadingMap({ ...loadingMap, [options.publishStatus]: true });
+    const res = await reqArticleUpdate({ id, ...options, ..._form, content: editor.getMarkdown() });
+    setLoadingMap({ ...loadingMap, [options.publishStatus]: false });
+    if (res.code != 200) return appNotification.error({ message: res.message || "更新失败！" });
+    appNotification.success({ message: res.message || "更新成功！" });
   };
-  const onPublish = async () => {
-    const _form = await form.validateFields().catch(() => undefined);
-    if (!_form || publishLoading) return;
-    setPublishLoading(true);
-    await sleep();
-    setPublishLoading(false);
-    // const editor = EditorRef.current?.getInstance();
-    // const s = editor?.getMarkdown();
-    // console.log("[s]", s);
-  };
+
   const [form] = Form.useForm();
   if (!isInitEnd) return <LazySpin />;
   //render
@@ -73,10 +63,14 @@ const ArticleManage: React.FC<ViewProps<Props>> = (props) => {
     <ContentMain className={`${className} flex-column`}>
       <ArticleCollapseForm
         form={form}
-        onDraft={onDraft}
-        draftLoading={draftLoading}
-        publishLoading={publishLoading}
-        onPublish={onPublish}
+        onDraft={() => {
+          onUpdate({ publishStatus: "draft" });
+        }}
+        draftLoading={loadingMap.draft}
+        publishLoading={loadingMap.publish}
+        onPublish={() => {
+          onUpdate({ publishStatus: "publish" });
+        }}
       />
       <main className="all-remain" style={{ marginTop: 10 }}>
         <Editor
